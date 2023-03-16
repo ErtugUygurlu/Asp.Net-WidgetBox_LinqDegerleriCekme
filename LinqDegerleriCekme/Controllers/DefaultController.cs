6de3370515a8ee382aa061d43311b3f76bb2d731
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-16 17:45:27 +0300

    Add project files.

---
+++ LinqDegerleriCekme/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LinqDegerleriCekme.Models.Entity;
+
+namespace LinqDegerleriCekme.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MuzikOgrenciKayitEntities db = new MuzikOgrenciKayitEntities();
+        public ActionResult Index()
+        {
+            var deger1 = db.TBLOGRENCILER.Count();
+            var deger2 = db.TBLOGRENCILER.Where(x => x.Enstruman == "gitar").Count();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            return View();
+        }
+    }
+}
